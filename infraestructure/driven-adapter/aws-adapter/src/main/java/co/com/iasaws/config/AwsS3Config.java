22b7c7f45837dd5b0eb0cf910ebdef842c8bd029
William Felipe Naranjo <felipe.naranjo@ias.com.co>
2026-03-27 11:57:01 -0500

    Ya carga el archivo

--- infraestructure/driven-adapter/aws-adapter/src/main/java/co/com/iasaws/config/AwsS3Config.java
+++ infraestructure/driven-adapter/aws-adapter/src/main/java/co/com/iasaws/config/AwsS3Config.java
@@ -4,8 +4,12 @@ import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+
+import java.net.URI;
 
 @Getter
 @Configuration
@@ -19,11 +23,20 @@ public class AwsS3Config {
 
     @Value("${aws.endpoint}")
     private String endpoint;
+
+    @Value("${aws.accessKey}")
+    private String accessKey;
+
+    @Value("${aws.secretKey}")
+    private String secretKey;
     
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
+                .endpointOverride(URI.create(endpoint))
                 .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
+                .forcePathStyle(true)
                 .build();
     }
 
